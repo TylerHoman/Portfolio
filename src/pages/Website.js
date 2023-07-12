2d4d972c08a35a9a6def797a9e56e1da47ff014a
TylerHoman <118566111+TylerHoman@users.noreply.github.com>
2023-07-12 12:43:50 -0700

    first commit?

---
+++ src/pages/Website.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import BtnSlider from "../components/Slider/BtnSlider.js";
+// import dataSlider from "./dataSlider";
+import Content from "../components/content.json";
+import Slider from "../components/Slider/slider";
+import Accordion from "../components/accordion";
+import AccordionReflect from "../components/accordionReflect.js";
+
+function MovieDatabase() {
+  const data = Content["movie-database"];
+  return (
+    <>
+      <section className="upper">
+        <div className="logo">
+          {Content && <img src={Content.icons[10]} alt="favicon" />}
+        </div>
+        <h2>{data.title}</h2>
+
+        <div className="slider">
+          <Slider data={data} />
+        </div>
+
+        <div className="contents">
+          <p>{data.contents}</p>
+        </div>
+      </section>
+    </>
+  );
+}
